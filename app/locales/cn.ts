be38b807e2a8405449773a53cd4a0a67629b21a1
coderFrankenstain <453628567@qq.com>
2023-05-12 21:57:36 +0800

    cjtoken

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,7 +4,7 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-      "访问密码不正确或为空，请前往[设置](/#/settings)页输入正确的访问密码，或者填入你自己的 OpenAI API Key。",
+      "请添加微信seakingweijun 获取使用权限，然后前往[设置](/#/settings)填入你自己的密钥。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
